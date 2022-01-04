2f5b0e9e9ffbfd73e34395c32dd4a2eeb297a3d8
Zheng Tao <ztao10001@gmail.com>
2022-01-04 15:57:21 -0500

    first assignment - hello world

---
+++ AlgorithmsBySedgewick/hello/HelloGoodbye.java
@@ -0,0 +1,18 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        System.out.print(args[0]);
+        System.out.print(" and ");
+        System.out.println(args[1] + ".");
+        System.out.print("Goodbye ");
+        System.out.print(args[1]);
+        System.out.print(" and ");
+        System.out.println(args[0] + ".");
+    }
+}
